4838f317ce99b1e868e2f950dc2937d053314b4d
damiangs <cgsdamian@gmail.com>
2026-06-06 17:26:46 -0600

    add Dockerfile and update package configurations for RabbitMQ integration

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,9 @@
+import amqp from "amqplib";
+
 async function main() {
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+
   console.log("Starting Peril server...");
 }
 
